c19f9a31aa8bc5f61303c3a17c0f49ec297118f0
Ondrej Roztocil <roztocil@outlook.com>
2024-04-08 10:28:44 +0200

    Initialize template

---
+++ README.md
@@ -0,0 +1,19 @@
+# Read Me
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To test:
+
+```bash
+bun test
+```
+
+To build:
+
+```bash
+bun run build
+```
---
+++ tests/some.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from "bun:test";
+
+describe("Something", () => {
+    test("isGood", () => {
+        expect(true).toBeTrue();
+    });
+});
